31e90e15fc9521f6c15960649fa2f25567d27877
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2020-11-21 18:02:49 +0000

    WIP

--- constants.lua
+++ constants.lua
@@ -1,6 +1,6 @@
 local Constants = {}
 
-Constants.ModName = "grade_seperated_rail_junction"
+Constants.ModName = "grade_saperated_rail_junction"
 Constants.AssetModName = "__" .. Constants.ModName .. "__"
 Constants.LogFileName = Constants.ModName .. "_logOutput.txt"
 
--- control.lua
+++ control.lua
@@ -1,8 +1,13 @@
+local GradedArea = require("scripts/graded-area")
+local EventScheduler = require("utility/event-scheduler")
+
 local function CreateGlobals()
+    GradedArea.CreateGlobals()
 end
 
 local function OnLoad()
-	--Any Remote Interface registration calls can go in here or in root of control.lua
+    --Any Remote Interface registration calls can go in here or in root of control.lua
+    GradedArea.OnLoad()
 end
 
 local function OnSettingChanged(event)
@@ -13,12 +18,12 @@ end
 
 local function OnStartup()
     CreateGlobals()
-	OnLoad()
+    OnLoad()
     OnSettingChanged(nil)
 end
-
 
 script.on_init(OnStartup)
 script.on_configuration_changed(OnStartup)
 script.on_event(defines.events.on_runtime_mod_setting_changed, OnSettingChanged)
 script.on_load(OnLoad)
+EventScheduler.RegisterScheduler()
