bda5a04c383e2bd3bbcf7af1f2eec2cfdc970682
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2021-01-04 22:18:28 +0000

    tests update

--- scripts/test-manager.lua
+++ scripts/test-manager.lua
@@ -8,14 +8,14 @@ local testsToDo
 if doTests then
     testsToDo = {
         tunnelSingleLoco = {enabled = false, testScript = require("tests/tunnel-single-loco")},
-        tunnelMultiWagonEastToWest = {enabled = true, testScript = require("tests/tunnel-multi-wagon-east-to-west")},
-        tunnelMultiWagonWestToEast = {enabled = true, testScript = require("tests/tunnel-multi-wagon-west-to-east")},
+        tunnelMultiWagonEastToWest = {enabled = false, testScript = require("tests/tunnel-multi-wagon-east-to-west")},
+        tunnelMultiWagonWestToEast = {enabled = false, testScript = require("tests/tunnel-multi-wagon-west-to-east")},
         surfaceSingleLoop = {enabled = false, testScript = require("tests/surface-single-loop")},
         surfaceMiddleLine = {enabled = false, testScript = require("tests/surface-middle-line")},
-        demo = {enabled = false, testScript = require("tests/demo")},
-        tunnelMultiWagonEastToWest2Tunnels = {enabled = true, testScript = require("tests/tunnel-multi-wagon-east-to-west-2-tunnels")},
-        tunnelMultiWagonNorthToSouth = {enabled = true, testScript = require("tests/tunnel-multi-wagon-north-to-south")},
-        tunnelMultiWagonNorthToSouth2Tunnels = {enabled = true, testScript = require("tests/tunnel-multi-wagon-north-to-south-2-tunnels")}
+        demo = {enabled = true, testScript = require("tests/demo")},
+        tunnelMultiWagonEastToWest2Tunnels = {enabled = false, testScript = require("tests/tunnel-multi-wagon-east-to-west-2-tunnels")},
+        tunnelMultiWagonNorthToSouth = {enabled = false, testScript = require("tests/tunnel-multi-wagon-north-to-south")},
+        tunnelMultiWagonNorthToSouth2Tunnels = {enabled = false, testScript = require("tests/tunnel-multi-wagon-north-to-south-2-tunnels")}
     }
 end
 
