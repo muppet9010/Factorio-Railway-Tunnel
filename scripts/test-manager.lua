6b7b5ec656cbf2bd7f66823bfc0e8ff68f62cf17
Muppet9010 <mike.hollett@gmail.com>
2022-01-21 22:15:29 +0000

    test setting clarification

--- scripts/test-manager.lua
+++ scripts/test-manager.lua
@@ -25,7 +25,7 @@ local Colors = require("utility/colors")
 local DoTests = true -- Enable test mode and does the enabled tests below if TRUE.
 
 local AllTests = false -- Does all the tests regardless of their enabled state below if TRUE.
-local ForceTestsFullSuite = false -- If true each test will do their full range, ignoring the tests "DoMinimalTests" setting. If false then each test just will honour their other settings.
+local ForceTestsFullSuite = false -- If true each test will do their full range, ignoring the tests "DoMinimalTests" setting, but honors their "DoSpecificTests" setting if enabled. If false then each test just will honour their other settings.
 
 local EnableDebugMode = true -- Enables debug mode when tests are run. Enables "railway_tunnel_toggle_debug_state" command.
 local WaitForPlayerAtEndOfEachTest = false -- The game will be paused when each test is completed before the map is cleared if TRUE. Otherwise the tests will run from one to the next. On a test erroring the map will still pause regardless of this setting.
